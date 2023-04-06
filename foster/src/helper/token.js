775ff08c8a8f16fad8ff55f93a4cfa9a48a7c388
Adarsh M <adarshm954@gmail.com>
2023-04-06 09:24:55 +0000

    changes

--- foster/src/helper/token.js
+++
@@ -1,7 +0,0 @@
-const userToken = localStorage.getItem("x-token");
-
-if (userToken) {
-    var userData = userToken ? JSON.parse(userToken):''
-}
-
-export default userData;
--- foster/src/store/sagas.js
+++
@@ -1,9 +0,0 @@
-import { all, fork } from "redux-saga/effects"
-import {userSaga} from "./users/saga"
-import authSaga from "./auth/saga"
-export default function* rootSaga() {
-  yield all([
-    fork(userSaga),
-    fork(authSaga)
-  ]);
-};
