c2dd3be1c0fbfeb14ef6c716e2eb39878b6ac52a
Adarsh M <Adarsh.M@resmed.com.au>
2023-04-24 18:07:43 +0530

    blog completed

--- src/store/index.js
+++ src/store/index.js
@@ -6,18 +6,27 @@ import postsReducer from "./postsSlice";
 import userReducer from "./userSlice";
 import categorySlice from "./categorySlice";
 
+// what is the root store?
+// the store that holds all the reducers
+
+// what are reducers?
+// reducers are functions that take in the state and action and return a new state
+
 const persistConfig = {
   key: "root",
   storage,
 };
+
 const rootReducer = combineReducers({
   posts: postsReducer,
   user: userReducer,
   categories: categorySlice,
 });
 
+// these are used to persist the state
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// the store that holds all the reducers and the middleware.
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: [thunk],
